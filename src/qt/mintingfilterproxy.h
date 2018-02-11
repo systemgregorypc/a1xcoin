86e733997474612404622279b8717588c0e55ea9
jose gregorio hernandez calderon <systemgregoryp.c@gmail.com>
2018-02-11 04:21:02 -0400

    Initial a1xcoin

--- src/qt/mintingfilterproxy.h
+++ src/qt/mintingfilterproxy.h
@@ -1,1 +1,14 @@
+#ifndef MINTINGFILTERPROXY_H
+#define MINTINGFILTERPROXY_H
+
+#include <QSortFilterProxyModel>
+
+class MintingFilterProxy : public QSortFilterProxyModel
+{
+    Q_OBJECT
+public:
+    explicit MintingFilterProxy(QObject *parent = 0);
+};
+
+#endif // MINTINGFILTERPROXY_H
 
