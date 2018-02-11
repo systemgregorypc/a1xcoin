91fb62df5e4d62e9e8e3915dc7acff0fd45409b0
jose gregorio hernandez calderon <systemgregoryp.c@gmail.com>
2018-02-11 03:56:15 -0400

    Add support for opening paycoin: URIs directly.

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define a1xcoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "a1xcoinURI"
+
+void ipcInit();
+void ipcShutdown();
+
+#endif // QTIPCSERVER_H
